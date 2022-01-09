552145d5832408aa9a430d24ac30f76492f4be14
Aerovent2 <leoheffel.87@gmail.com>
2022-01-09 14:55:36 -0300

    clase4-simulador

--- README.md
+++ README.md
@@ -1,6 +1,184 @@
 # js 
 Heffel Leonardo
 pedacitos de codigo y anotaciones
+*************************************************************************************************************************************************************
+//calcula que dias son habiles segun la cantidad de dias del mes y en que dia comienza el mes
+let cantidadDias = parseInt(prompt("Cuantos dias tiene el mes"));
+let primerDia;
+let dato1=false;
+let dato2=false;
+
+if (cantidadDias != null && cantidadDias >= 28 && cantidadDias<=31 ){
+    primerDia = prompt("cual es el primer dia del mes");
+    dato1=true;
+    }
+else{
+    alert("Ingrese un Numero ente 28 y 31");
+
+}
+
+if (primerDia != null ){
+    primerDia = primerDia.toLowerCase();
+    dato2=true;
+    }
+else{
+    alert("ingrese un dia válido (Lunes, Jueves, etc)");
+
+}
+if (dato1&&dato2){
+    let lun = 0
+    let mar = 1
+    let mie = 2
+    let jue = 3
+    let vie = 4
+    let sab = 5
+    let dom = 6
+    let dia = "dia"
+    switch(primerDia){
+        case "lunes":
+            lun = 0;
+            mar = 1;
+            mie = 2;
+            jue = 3;
+            vie = 4;
+            sab = 5;
+            dom = 6;
+            break;
+        case "martes":
+            lun = 6;
+            mar = 0;
+            mie = 1;
+            jue = 2;
+            vie = 3;
+            sab = 4;
+            dom = 5;
+            break;
+        case "miercoles":
+            lun = 5;
+            mar = 6;
+            mie = 0;
+            jue = 1;
+            vie = 2;
+            sab = 3;
+            dom = 4;
+            break;        
+        case "jueves":
+            lun = 4;
+            mar = 5;
+            mie = 6;
+            jue = 0;
+            vie = 1;
+            sab = 2;
+            dom = 3;
+            break;
+        case "viernes":
+            lun = 3;
+            mar = 4;
+            mie = 5;
+            jue = 6;
+            vie = 0;
+            sab = 1;
+            dom = 2;
+            break;
+        case "sabado":
+            lun = 2;
+            mar = 3;
+            mie = 4;
+            jue = 5;
+            vie = 6;
+            sab = 0;
+            dom = 1;
+            break;
+        case "domingo":
+            lun = 1;
+            mar = 2;
+            mie = 3;
+            jue = 4;
+            vie = 5;
+            sab = 6;
+            dom = 0;
+            break;
+        default:
+        alert("Supongamos que dijiste Lunes");
+    }
+
+
+    for (let i=1; i <= cantidadDias; i++){
+        if (i == 1){
+            lun = lun + 1;
+            mar = mar + 1;
+            mie = mie + 1;
+            jue = jue + 1;
+            vie = vie + 1;
+            sab = sab + 1;
+            dom = dom + 1;
+            
+        }
+        else if (i == 7 ){
+            lun = lun + 7;
+            mar = mar + 7;
+            mie = mie + 7;
+            jue = jue + 7;
+            vie = vie + 7;
+            sab = sab + 7;
+            dom = dom + 7;
+            
+        }
+        else if (i == 14 ){
+            lun = lun + 7;
+            mar = mar + 7;
+            mie = mie + 7;
+            jue = jue + 7;
+            vie = vie + 7;
+            sab = sab + 7;
+            dom = dom + 7;
+            
+        }
+        else if (i == 21 ){
+            lun = lun + 7;
+            mar = mar + 7;
+            mie = mie + 7;
+            jue = jue + 7;
+            vie = vie + 7;
+            sab = sab + 7;
+            dom = dom + 7;
+            
+        }
+        else if (i == 28 ){
+            lun = lun + 7;
+            mar = mar + 7;
+            mie = mie + 7;
+            jue = jue + 7;
+            vie = vie + 7;
+            sab = sab + 7;
+            dom = dom + 7;
+            
+        }
+        if (lun == i){
+            dia = "Lunes";
+        }
+        if (mar == i){
+            dia = "Martes";
+        }
+        if (mie == i){
+            dia = "Miercoles";
+        }
+        if (jue == i){
+            dia = "Jueves";
+        }
+        if (vie == i){
+            dia = "Viernes";
+        }
+
+        if (i == sab || i == dom){
+            console.log("Fin de Semana")
+            continue;
+        }
+        console.log(`${dia} ${i} habil`)
+
+    }
+}
+********************************************************************************************************************
 
 //devuelve la decada en que naciste
 
@@ -45,7 +223,7 @@ else {
     console.log("ingrese un dato valido")
 };
 
-
+*****************************************************************************************************************************************
 
 
 //revisa si es primo
@@ -72,7 +250,7 @@ else {
         console.log("el numero es primo")
     }
 }
-
+***********************************************************************************************************************************************
 
 //siempre console log para ver que hace
 //poner else para error de datos
@@ -86,7 +264,7 @@ if (numero%2 == 0){
 else {
     console.log("el numero no es par")
 }
-
+*************************************************************************************************************************
 //un semaforo
 let luz = prompt("que luz tiene el semaforo").toLowerCase()
 if (luz == "verde"){
@@ -101,7 +279,7 @@ else if (luz == "rojo"){
 else {
     console.log ("el semaforo esta roto")
 }
-
+***********************************************************************************************************
 
 let horasExtras = true;
 if (horasExtras){
--- main.js
+++ main.js
@@ -1,177 +1,58 @@
 
-//calcula que dias son habiles segun la cantidad de dias del mes y en que dia comienza el mes
-let cantidadDias = parseInt(prompt("Cuantos dias tiene el mes"));
-let primerDia;
-let dato1=false;
-let dato2=false;
+//calcula el costo de combustible segun los datos ingresados
+let kilometrosPorLitro;
+let consumoCadaCien;
+let consumoPorKilometro;
+let distanciaTotal;
+let precioCombustible =  parseInt(prompt("Ingrese el precio por litro"));
+let medicion = parseInt(prompt("como desea ingresar el consumo? solo números \n 1-(km/l) \n 2-(l/100km)"));
+let distanciaParcial = parseInt(prompt("ingrese la distancia en kilometros"));
+let idaYvuelta = prompt("viaja ida y vuelta (S/N)");
+let correcto= true;
 
-if (cantidadDias != null && cantidadDias >= 28 && cantidadDias<=31 ){
-    primerDia = prompt("cual es el primer dia del mes");
-    dato1=true;
-    }
-else{
-    alert("Ingrese un Numero ente 28 y 31");
 
+if(medicion == "1"){
+    kilometrosPorLitro= parseInt(prompt("ingrese rendimiento por litro"));
+    consumoPorKilometro =  1/kilometrosPorLitro;
 }
-
-if (primerDia != null ){
-    primerDia = primerDia.toLowerCase();
-    dato2=true;
-    }
-else{
-    alert("ingrese un dia válido (Lunes, Jueves, etc)");
-
+else if(medicion == "2"){
+    consumoCadaCien = parseInt(prompt("ingrese el consumo cada 100 kilometros (l/100)"));
+    consumoPorKilometro = consumoCadaCien/100;
 }
-if (dato1&&dato2){
-    let lun = 0
-    let mar = 1
-    let mie = 2
-    let jue = 3
-    let vie = 4
-    let sab = 5
-    let dom = 6
-    let dia = "dia"
-    switch(primerDia){
-        case "lunes":
-            lun = 0;
-            mar = 1;
-            mie = 2;
-            jue = 3;
-            vie = 4;
-            sab = 5;
-            dom = 6;
+    else{
+        correcto = false;
+        alert("No ingreso medicion valida");
+    }
+
+idaYvuelta = idaYvuelta.toLocaleLowerCase(); 
+
+
+if (idaYvuelta != "s" && idaYvuelta != "n"){
+    correcto=false
+    alert("No ingreso un dato ida/vuelta valido");
+} 
+
+if (correcto){
+    switch(idaYvuelta){
+        case "s":
+            distanciaTotal = distanciaParcial *2;
             break;
-        case "martes":
-            lun = 6;
-            mar = 0;
-            mie = 1;
-            jue = 2;
-            vie = 3;
-            sab = 4;
-            dom = 5;
-            break;
-        case "miercoles":
-            lun = 5;
-            mar = 6;
-            mie = 0;
-            jue = 1;
-            vie = 2;
-            sab = 3;
-            dom = 4;
-            break;        
-        case "jueves":
-            lun = 4;
-            mar = 5;
-            mie = 6;
-            jue = 0;
-            vie = 1;
-            sab = 2;
-            dom = 3;
-            break;
-        case "viernes":
-            lun = 3;
-            mar = 4;
-            mie = 5;
-            jue = 6;
-            vie = 0;
-            sab = 1;
-            dom = 2;
-            break;
-        case "sabado":
-            lun = 2;
-            mar = 3;
-            mie = 4;
-            jue = 5;
-            vie = 6;
-            sab = 0;
-            dom = 1;
-            break;
-        case "domingo":
-            lun = 1;
-            mar = 2;
-            mie = 3;
-            jue = 4;
-            vie = 5;
-            sab = 6;
-            dom = 0;
+        case "n":
+            distanciaTotal = distanciaParcial;
             break;
         default:
-        alert("Supongamos que dijiste Lunes");
+            distanciaTotal = distanciaParcial;
+            console.log("no deberias estar viendo esto");
+        }
+
+    console.log(`${consumoPorKilometro} consumo por kilometro ${distanciaTotal} distancia total ${precioCombustible} `);
+
+    const costo = (precio, consumo, distancia) => {
+        return precio * (consumo * distancia);
     }
 
-
-    for (let i=1; i <= cantidadDias; i++){
-        if (i == 1){
-            lun = lun + 1;
-            mar = mar + 1;
-            mie = mie + 1;
-            jue = jue + 1;
-            vie = vie + 1;
-            sab = sab + 1;
-            dom = dom + 1;
-            
-        }
-        else if (i == 7 ){
-            lun = lun + 7;
-            mar = mar + 7;
-            mie = mie + 7;
-            jue = jue + 7;
-            vie = vie + 7;
-            sab = sab + 7;
-            dom = dom + 7;
-            
-        }
-        else if (i == 14 ){
-            lun = lun + 7;
-            mar = mar + 7;
-            mie = mie + 7;
-            jue = jue + 7;
-            vie = vie + 7;
-            sab = sab + 7;
-            dom = dom + 7;
-            
-        }
-        else if (i == 21 ){
-            lun = lun + 7;
-            mar = mar + 7;
-            mie = mie + 7;
-            jue = jue + 7;
-            vie = vie + 7;
-            sab = sab + 7;
-            dom = dom + 7;
-            
-        }
-        else if (i == 28 ){
-            lun = lun + 7;
-            mar = mar + 7;
-            mie = mie + 7;
-            jue = jue + 7;
-            vie = vie + 7;
-            sab = sab + 7;
-            dom = dom + 7;
-            
-        }
-        if (lun == i){
-            dia = "Lunes";
-        }
-        if (mar == i){
-            dia = "Martes";
-        }
-        if (mie == i){
-            dia = "Miercoles";
-        }
-        if (jue == i){
-            dia = "Jueves";
-        }
-        if (vie == i){
-            dia = "Viernes";
-        }
-
-        if (i == sab || i == dom){
-            console.log("Fin de Semana")
-            continue;
-        }
-        console.log(`${dia} ${i} habil`)
-
-    }
+    alert("Vas a gastar $"+ Math.round(costo(precioCombustible,consumoPorKilometro,distanciaTotal)));
+}
+else {
+    console.log("ingresaste algo mal")
 }
